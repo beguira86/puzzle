b320f70e7d17c00359c5dd3a179b57d331bcdc48
Carl Westmoreland <beguira86@gmail.com>
2015-11-15 14:09:07 -0500

    blag

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,11 @@
 class GamesController < ApplicationController
 	def index
-		@games = Game.where(puzzle_id: params[:id])   #.order("time").limit(10)
+		@games = Game.where(puzzle_id: params[:id]).order("time").limit(10)
 		render "index.json.jbuilder", status: :ok
 	end
 
 	def save
-		@game = current_user.games.new(time: params[:time], puzzle_id: :id)
+		@game = current_user.games.new(time: params[:time], puzzle_id: params[:id])
 	#	@game = Game.new(user: user.id, time: params[:time], puzzle_id: :id)
 		if @game.save
 			render json: { puzzle: @game }, status: :ok
